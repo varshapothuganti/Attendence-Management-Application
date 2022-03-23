21ddbecdf573a2d96fb64f5ea23cd224cd185bfb
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-23 10:06:14 +0530

    Update AttendenceDAO.java

--- src/main/java/com/cg/ams/repository/AttendenceDAO.java
+++ src/main/java/com/cg/ams/repository/AttendenceDAO.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.bean.Attendance;
+
 @Repository
 public interface IAttendanceRepository extends JpaRepository<Attendance, Long> {
 	
