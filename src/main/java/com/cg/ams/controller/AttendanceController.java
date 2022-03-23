eff7712118f662cf04ae7b3d07f1f44e1af3f456
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-23 10:36:00 +0530

    Update AttendanceController.java

--- src/main/java/com/cg/ams/controller/AttendanceController.java
+++ src/main/java/com/cg/ams/controller/AttendanceController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.bean.Attendance;
-import com.example.demo.service.AttendanceServiceImpl;
-import com.example.demo.service.IAttendanceService;
+import com.cg.ams.entity.Attendance;
+import com.cg.ams.service.AttendanceServiceImpl;
+import com.cg.ams.service.IAttendanceService;
 
 @RestController
 public class AttendanceController {
