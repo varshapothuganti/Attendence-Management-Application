403753ba884cc7c5806c569887c4a4b19e52c728
Adavelly Naveen Reddy <84331167+naveen26887@users.noreply.github.com>
2022-03-23 16:51:03 +0530

    Added SubjectService

---
+++ src/main/java/com/cg/ams/service/SubjectService.java
@@ -0,0 +1,25 @@
+package com.example.demo.com.cg.ams.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.com.cg.ams.entity.SubjectEntity;
+import com.example.demo.com.cg.ams.exception.RecordNotFoundException;
+import com.example.demo.com.cg.ams.repository.SubjectDAO;
+@Service
+public interface SubjectService {
+	
+
+	public long add(SubjectEntity entity);
+	public void update(SubjectEntity entity) throws RecordNotFoundException;
+	public void delete(SubjectEntity entity) throws RecordNotFoundException;
+	public SubjectEntity findByName(String name) throws Exception;
+	public SubjectEntity findByPk(long id) throws RecordNotFoundException;
+	public List<SubjectEntity> search(SubjectEntity entity, long pageNo, int pageSize);
+	public List<SubjectEntity> search(SubjectEntity entity);
+	public List<SubjectEntity> getAllSubjects();
+	
+	
+}
