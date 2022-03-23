b7e1a660c366da406553a0f451ca87faf9409bc3
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-23 10:19:20 +0530

    Update AttendanceController.java

--- src/main/java/com/cg/ams/controller/AttendanceController.java
+++ src/main/java/com/cg/ams/controller/AttendanceController.java
@@ -1,4 +1,53 @@
 package com.cg.ams.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.Attendance;
+import com.example.demo.service.AttendanceServiceImpl;
+import com.example.demo.service.IAttendanceService;
+
+@RestController
 public class AttendanceController {
+	@Autowired
+	IAttendanceService attServ;
+	
+	@PostMapping("/attendance")
+	ResponseEntity<Attendance> add(@RequestBody Attendance att) {
+		Attendance newAtt =  attServ.add(att);
+		return new ResponseEntity<>(newAtt, HttpStatus.CREATED);
+	}
+		
+	@GetMapping("/attendance/{id}")
+	Attendance findByPk(@PathVariable("id") int id)
+	{
+		return attServ.findByPk(id);
+	}
+	@GetMapping("/attendance/byname/{student_name}")
+	Attendance findByPk(@PathVariable("student_name") String student_name)
+	{
+		Attendance newAtt = attServ.findByName(student_name);
+		return newAtt;
+	}
+	@DeleteMapping("/attendance/entity")
+	void delete(Attendance entity) {
+		attServ.delete(entity);
+		
+	}
+	@PutMapping(path ="/updateattendance")
+	public void update(@RequestBody Attendance att)  {
+		attServ.update(att);
+
+		
+		
+	}
+
 }
