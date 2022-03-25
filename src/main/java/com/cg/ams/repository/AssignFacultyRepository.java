16c05d98b4416c4fafb3444249220045af084832
RamuK2001 <85925640+RamuK2001@users.noreply.github.com>
2022-03-25 09:44:59 +0530

    Added AssignFacultyRepository

--- src/main/java/com/cg/ams/repository/AssignFacultyRepository.java
+++ src/main/java/com/cg/ams/repository/AssignFacultyRepository.java
@@ -1,1 +1,13 @@
+package com.cg.ams.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.ams.entity.AssignFacultyEntity;
+
+@Repository
+public interface AssignFacultyRepository extends JpaRepository<AssignFacultyEntity,Long>{
+	
+	public AssignFacultyEntity findByUserName(String name);
+	
+}
