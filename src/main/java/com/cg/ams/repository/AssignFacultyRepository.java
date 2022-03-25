ecbd26d8da5d186066ba208ca374cf910db76c01
RamuK2001 <85925640+RamuK2001@users.noreply.github.com>
2022-03-25 14:53:48 +0530

    Added AssignFacultyRepository

--- src/main/java/com/cg/ams/repository/AssignFacultyRepository.java
+++ src/main/java/com/cg/ams/repository/AssignFacultyRepository.java
@@ -1,5 +1,8 @@
 package com.cg.ams.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,7 @@ import com.cg.ams.entity.AssignFacultyEntity;
 public interface AssignFacultyRepository extends JpaRepository<AssignFacultyEntity,Long>{
 	
 	public AssignFacultyEntity findByUserName(String name);
+	public List<AssignFacultyEntity> findByUserNameIgnoreCase(String name,Pageable pageable);
+	public List<AssignFacultyEntity> findByUserNameIgnoreCase(String name);
 	
 }
